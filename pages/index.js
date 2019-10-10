eefc8b12fdc4bc499757946be5d93b051ff59116
chuxu <chuxu@chuxudeMacBook-Pro-2.local>
2019-10-10 20:22:49 +0800

    增加FormRegister

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
-
 import styles from './index.css';
-
+import FormRegister from './FormRegister';
 export default function() {
   return (
     <div className={styles.normal}>
       <h1>Page index</h1>
+      <FormRegister />
     </div>
   );
 }
